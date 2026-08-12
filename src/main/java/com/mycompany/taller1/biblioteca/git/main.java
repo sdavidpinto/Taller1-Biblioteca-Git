86f161c52df3a28b3f3db73b505de10b4c92ca3c
sdavidpinto <sdavidpinto@unicesar.edu.co>
2026-08-12 10:36:05 -0500

    fix: correcion de inicializacion de proyecto maven

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller1.biblioteca.git;
+
+/**
+ *
+ * @author Samue
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
