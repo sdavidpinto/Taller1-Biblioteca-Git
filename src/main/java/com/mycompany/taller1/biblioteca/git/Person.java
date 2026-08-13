3dd02e05378efa6700fe4ac9aa7b4a2d5e3212f4
sdavidpinto <sdavidpinto@unicesar.edu.co>
2026-08-13 15:37:48 -0500

    Feat:Crear clase Persona como padre de Cliente

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Person.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller1.biblioteca.git;
+
+/**
+ *
+ * @author Samue
+ */
+public abstract class Person {
+    private String id;
+    private String name;
+    private String phoneNumber;
+
+    public Person(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    public abstract String Show();
+    
+}
